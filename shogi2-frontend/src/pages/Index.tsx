5b6db189ceb976bf4fa3ec238289dae07a326403
kamui-akatuki <kamui-akatuki@kamui-akatukinoMacBook-Air.local>
2025-06-20 21:51:50 +0900

    部屋を作るところ作り始めた

--- shogi2-frontend/src/pages/Index.tsx
+++ shogi2-frontend/src/pages/Index.tsx
@@ -1,9 +1,17 @@
 import "./styles/Index.css";
 import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 export default function Index(){
   return (
     <div className="index">
+      <div className="match-box">
+        <TextField variant="standard" fullWidth label="部屋番号"/>
+        <div className="match-box-buttons">
+          <Button className="match-box-button">部屋を作る</Button>
+          <Button className="match-box-button">部屋に入る</Button>
+        </div>
+      </div>
     </div>
   );
 }
