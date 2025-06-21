f7845f7c6ea77186240649dc10d02301f782c6db
kamui-akatuki <kamui-akatuki@kamui-akatukinoMacBook-Air.local>
2025-06-21 10:45:51 +0900

    https://x.com/_kamui_akatuki

--- shogi2-frontend/src/components/common/Piece.tsx
+++ shogi2-frontend/src/components/common/Piece.tsx
@@ -1,20 +1,28 @@
 import "./styles/Piece.css";
 import pieceImg from "../../assets/piece.png";
-import {boardAtom,playerAtom,turnAtom} from "../../state";
-import {useAtomValue} from "jotai";
+import {boardAtom,playerAtom,turnAtom,focusedPieceAtom} from "../../state";
+import {useAtomValue,useAtom} from "jotai";
 import {useMemo} from "react";
 
 export default function Piece({pos}:{pos:number[]}){
   const board=useAtomValue(boardAtom);
   const player=useAtomValue(playerAtom);
   const turn=useAtomValue(turnAtom);
+  const [focusedPiece,setFocusedPiece]=useAtom(focusedPieceAtom);
   const piece=useMemo(()=>board[pos[0]][pos[1]].piece,board);
   const onFocus=()=>{
+    if (!piece){
+      return;
+    }
+    if (turn===player && piece.player===player){
+      setFocusedPiece({pos,piece});
+    }
   };
   return (
-    <div className={`piece ${piece?.player===player?"":"turn"} ${turn===player && piece?.player===player?"movable":""}`} onClick={onFocus}>
+    <div className={`piece ${piece?.player===player?"":"turn"} ${turn===player && piece?.player===player?"movable":""} ${pos===focusedPiece?.pos?"focus":""}`} onClick={onFocus}>
       <img src={pieceImg}/>
       <p>{piece?.type.name}</p>
+      <div className="cover"/>
     </div>
   );
 }
--- shogi2-frontend/src/state.ts
+++ shogi2-frontend/src/state.ts
@@ -14,5 +14,6 @@ const boardAtom=atom<board>([
 const playerAtom=atom<"player1"|"player2"|undefined>("player1");
 const turnAtom=atom<"player1"|"player2">("player1");
 const wsAtom=atom<WebSocket|null>(null);
+const focusedPieceAtom=atom<{pos:number[],piece:piece}|null>(null);
 
-export {boardAtom,playerAtom,turnAtom,wsAtom};
+export {boardAtom,playerAtom,turnAtom,wsAtom,focusedPieceAtom};
