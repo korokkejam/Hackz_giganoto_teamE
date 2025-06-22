a0c46de722b270dd453c5e597e2d2ef6eec98a6b
Reo-Metasura <130132reodora@gmail.com>
2025-06-22 10:15:05 +0900

    cococomaiを放出

--- mods/commandExecutor.ts
+++ mods/commandExecutor.ts
@@ -1,21 +1,23 @@
 import { CommandResult, GameState } from './commonTypes';
 import { CommandBase } from './base/CommandBase';
-import { TeleportCommand } from './teleportation/TeleportCommand';
+import { CococomaiCommand } from './cococomai/CococomaiCommand';
 import { KillCommand } from './kill/KillCommand';
 import { PlaySoundCommand } from './playsound/PlaySoundCommand';
-
+import { TeleportCommand } from './teleportation/TeleportCommand';
 // ここに新しいクラスをインポート
 
 // Command execution function
 export function createCommand<TCommand extends {type: string}, TResult = undefined>(raw: TCommand, gameState: GameState): CommandBase<TResult> {
   
   switch (raw.type) {
-    case 'teleport':
-      return new TeleportCommand(raw, gameState) as unknown as CommandBase<TResult>;
+    case 'cococomai':
+      return new CococomaiCommand(raw, gameState) as unknown as CommandBase<TResult>;
     case 'kill':
       return new KillCommand(raw, gameState) as unknown as CommandBase<TResult>;
     case 'playSound':
       return new PlaySoundCommand(raw, gameState) as unknown as CommandBase<TResult>;
+    case 'teleport':
+      return new TeleportCommand(raw, gameState) as unknown as CommandBase<TResult>;
     // ここに新しいcaseを追加
     
     default:
